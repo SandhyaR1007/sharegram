58437040256d0b90eb74069c5d63dc21b821b83e
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-01-22 00:50:30 +0530

    add: Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Stories from "../components/Stories";
+import Post from "../components/Post";
+
+const Home = () => {
+  return (
+    <div>
+      <Stories />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Home;
